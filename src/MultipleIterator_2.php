97ba9cc143aed608dc0a6300d683e12d39d39a66
duplabe <duplabe@gmail.com>
2011-12-05 21:38:36 +0100

    rename MultipleIterator files

--- src/MultipleIterator_2.php
+++ src/MultipleIterator_2.php
@@ -5,12 +5,12 @@ $arrayIterator2 = new ArrayIterator(array('5','6','7',));
 $arrayIterator3 = new ArrayIterator(array('8','9','10','11',));
 
 $miterator = new MultipleIterator(
-    MultipleIterator::MIT_KEYS_ASSOC | MultipleIterator::MIT_NEED_ALL
+    MultipleIterator::MIT_KEYS_NUMERIC | MultipleIterator::MIT_NEED_ANY
 );
 $miterator->attachIterator($arrayIterator1, 'foo');
 $miterator->attachIterator($arrayIterator2, 'bar');
 $miterator->attachIterator($arrayIterator3, 'baz');
 
 foreach ($miterator as $item) {
-    echo var_export($item, true), PHP_EOL;
+    print_r($item);
 }
